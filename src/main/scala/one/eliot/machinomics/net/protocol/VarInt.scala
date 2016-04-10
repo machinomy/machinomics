0a1c27406c8b1c19cd72ddee82f0ced93680b936
Sergey Ukustov <sergey@ukstv.me>
2016-04-10 19:16:43 +0300

    Move around internal functions

--- src/main/scala/one/eliot/machinomics/net/protocol/VarInt.scala
+++ src/main/scala/one/eliot/machinomics/net/protocol/VarInt.scala
@@ -9,23 +9,23 @@ case class VarInt(v: BigInt) {
 }
 
 object VarInt {
-  def longToVarInt(unsignedLong: Long): VarInt = VarInt(longToBigInt(unsignedLong))
-
-  def longToBigInt(unsignedLong: Long): BigInt = (BigInt(unsignedLong >>> 1) << 1) + (unsignedLong & 1)
-
-  def bigIntToLong(n: BigInt): Long = {
-    val smallestBit = (n & 1).toLong
-    ((n >> 1).toLong << 1) | smallestBit
-  }
-
-  def encode(varInt: VarInt): Attempt[BitVector] = varInt.v match {
-    case v if v < 0xFD => uint8L.encode(v.toInt)
-    case v if v <= 0xFFFF => uint16L.encode(v.toInt).map(e => BitVector(0xFD) ++ e)
-    case v if v <= 0xFFFFFFFF => uint32L.encode(v.toInt).map(e => BitVector(0xFE) ++ e)
-    case v => int64L.encode(bigIntToLong(v))
+  def encode(varInt: VarInt): Attempt[BitVector] = {
+    def bigIntToLong(n: BigInt): Long = {
+      val smallestBit = (n & 1).toLong
+      ((n >> 1).toLong << 1) | smallestBit
+    }
+    varInt.v match {
+      case v if v < 0xFD => uint8L.encode(v.toInt)
+      case v if v <= 0xFFFF => uint16L.encode(v.toInt).map(e => BitVector(0xFD) ++ e)
+      case v if v <= 0xFFFFFFFF => uint32L.encode(v.toInt).map(e => BitVector(0xFE) ++ e)
+      case v => int64L.encode(bigIntToLong(v))
+    }
   }
 
   def decode(bitVector: BitVector): Attempt[DecodeResult[VarInt]] = {
+    def longToBigInt(unsignedLong: Long): BigInt = (BigInt(unsignedLong >>> 1) << 1) + (unsignedLong & 1)
+    def longToVarInt(unsignedLong: Long): VarInt = VarInt(longToBigInt(unsignedLong))
+
     bitVector.toByteArray.toList match {
       case -1 :: v => int64L.decode(BitVector(v)).map(_.map(longToVarInt))
       case -2 :: v => uint32L.decode(BitVector(v)).map(_.map(longToVarInt))
